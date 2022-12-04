f50c1dd02b964a7887ca517a5a81c24692219ba2
ema.j <ema.j097@gmail.com>
2022-12-04 13:09:16 +0100

    added cat emoji on the fact card

--- src/components/RandomFactCard/RandomFactCard.tsx
+++ src/components/RandomFactCard/RandomFactCard.tsx
@@ -3,7 +3,7 @@ import "./RandomFactCard.scss";
 const RandomFactCard = ({ randomFact }: any) => {
   return (
     <div className="randomFactContainer">
-      <span>Random Cat Fact</span>
+      <span>Random Cat Fact &#x1F431;</span>
       <h2>❝{randomFact}❞</h2>
     </div>
   );
